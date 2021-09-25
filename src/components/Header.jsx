9479dfee73870c1d5c4cef6c0ff7157c35eeec43
michaelsatumba <80368334+michaelsatumba@users.noreply.github.com>
2021-09-24 17:10:06 -0700

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <VpnKeyIcon /> Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
